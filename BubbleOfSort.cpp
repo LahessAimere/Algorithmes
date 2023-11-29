8372432821fe1e7e2332d62aa6bca51f66b8366f
Lahess <80914959+LahessAimere@users.noreply.github.com>
2023-11-29 23:19:42 +0100

    Added an array of variable size

--- BubbleOfSort.cpp
+++ BubbleOfSort.cpp
@@ -18,6 +18,11 @@ void bubbleSortAscending(int* tab, int size)
             }
         }
     }
+     for (int i = 0; i < size; i++) 
+    {
+        
+        std::cout << tab[i] << std::endl;
+    }
 }
 
 void bubbleSortDescending(int* tab, int size)
@@ -36,27 +41,75 @@ void bubbleSortDescending(int* tab, int size)
             }
         }
     }
-}
-
-void displayIntArray(int* tab, int size) 
-{
-    for (int i = 0; i < size; i++) 
+     for (int i = 0; i < size; i++) 
     {
+        
         std::cout << tab[i] << std::endl;
     }
 }
 
-int main()
+void TableAlreadyImplemented()
 {
     int tab[] = {5,2,7,4,9};
     int size = 5;
 
     std::cout << " Ordre Croissant "<< std::endl;
     bubbleSortAscending(tab, size);
-    displayIntArray(tab, size);
 
     std::cout << " Ordre Décroissant " << std::endl;
     bubbleSortDescending(tab, size);
-    displayIntArray(tab, size);
+     std::cout << " " << std::endl;
+}
+
+void ManualLayoutTable()
+{
+    int size;
+    int* tab;
+
+    char choice;
+    std::cout << "Veux tu un tableau en ordre croissant (c) ou décroissant (d) ?" << std::endl;
+    std::cin >> choice;
+
+    if (choice == 'c' || choice == 'C') 
+    {
+        std::cout << "Entrez la taille du tableau : ";
+        std::cin >> size;
+
+        tab = new int[size];
+
+        std::cout << "Entrez les éléments du tableau (séparés par des espaces) : ";
+        for (int i = 0; i < size; ++i) 
+        {
+            std::cin >> tab[i];
+        }
+        bubbleSortAscending(tab, size);
+    }
+    else if (choice == 'd' || choice == 'D')
+    {
+        std::cout << "Entrez la taille du tableau : ";
+        std::cin >> size;
+
+        tab = new int[size];
+
+        std::cout << "Entrez les éléments du tableau (séparés par des espaces) : ";
+        for (int i = 0; i < size; ++i) 
+        {
+            std::cin >> tab[i];   
+        }
+        
+        bubbleSortDescending(tab, size);
+    }
+    else 
+    {
+        std::cout << "Choix non valide." << std::endl;
+    }
+    delete[] tab;
+}
+
+int main()
+{
+    TableAlreadyImplemented();
+    ManualLayoutTable();
+
     return 0;
 }
